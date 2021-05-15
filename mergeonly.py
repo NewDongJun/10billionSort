1262f19b8b5c24431a2156686ade5cc52182f3d1
ShinDongJun <shindj4613@outlook.com>
2021-05-15 15:40:39 +0900

    dd

--- mergeonly.py
+++ mergeonly.py
@@ -1,29 +1,56 @@
 import array
-import pickle
 import time
 import heapq
 
 sortedfilename = '10billion.sorted'
 datatype = 'i'
 chunksize = 10000000
+arraysize = 10000
 
 chunkcnt = 101 #100개 인데 원래 파일 에서는 +1 하고 끝남
-chunckReadList = []
+chunkReadList = []
 start = time.time()
 
 #성능 좋은 합치는 제너레이터 만들기
-def 
+def chunkread(filename):
+    f = open(filename, 'rb')
+    data = array.array(datatype)
+    try:
+        while True:
+            data  = array.array(datatype)
+            #이거는 원래 청크에서도 실험해 볼 필요가 있음
+            data.fromfile(f,arraysize)
+            for i in data:
+                yield i
+    except :
+        if len(data) >0:
+            for i in data:
+                yield i
+        else:
+            pass
 
 
 #chunk들을 읽어올 객체 리스트 만들기
 for i in range(1,chunkcnt):
     file = 'chunk'+str(i)+'.pickle'
-    chunckReadList.append(chunckread(file))
+    chunkReadList.append(chunkread(file))
 
 #chunk들 읽어 오면서 정렬하기
 f = open(sortedfilename, 'wb')
-merge_data = heapq.merge(*chunckReadList)
-for i in merge_data:
-    pickle.dump(i, f)
-f.close()
+merge_data = heapq.merge(*chunkReadList)
+try:
+    block = None
+    while True:
+        block = array.array(datatype)
+        for i in range(arraysize):
+            block.append(next(merge_data))
+        block.tofile(f)
+except:
+    if len(block)>0:
+        block.tofile(f)
+    else:
+        pass
+finally:
+    f.close()
+
 print('endtime :',time.time()-start)
--- readAndDivision.py
+++ readAndDivision.py
@@ -1,13 +1,11 @@
 import array
 import pickle
 import time
-import heapq
-from typing_extensions import ParamSpec
 
 filename = '10billion.int'
 sortedfilename = '10billion.sorted'
 datatype = 'i'
-chunksize = 10000000
+chunksize = 10000000 #약160메가 좀 넘는 용량
 arraysize = 10000
 
 chunkcnt = 1
@@ -23,20 +21,35 @@ while True:
     try:
         for i in range(chunksize):
             data.append(pickle.load(f))
-        print('read chunk'+str(chunkcnt)+':', time.time()-start)
     except EOFError:
         if len(data)>0: #남아 있는 데이터가 chunk사이즈 보다 작을 때
             pass
         else:           #남아 있는 데이터가 없을 경우
             break
+    print('read chunk'+str(chunkcnt)+':', time.time()-start)
+    
 
     #sort
     sortedData = sorted(data)
     print('sort chunk'+str(chunkcnt)+':', time.time()-start)
 
+
     #store 이 부분에서 데이터를 어레이로 묶어서 묶은 단위 만큼 저장
     chunk = open('chunk'+str(chunkcnt)+'.pickle','wb')
-    block = array.array(datatype)
-    
-
+    try:
+        block = None
+        while True:
+            block = array.array(datatype)
+            for i in range(arraysize):
+                block.append(sortedData.pop(0))
+            block.tofile(chunk) #어레이 사이즈 만큼 읽어서 파일에 한번에 저장
+    except:
+        if len(block)>0: #남은데이터가 arraysize 보다 작은경우
+            block.tofile(chunk)
+        else:            #남은 데이터가 없는 경우
+            pass
+    finally:
+        chunk.close()
+        print('store chunk'+str(chunkcnt)+':', time.time()-start)
+        chunkcnt += 1
 f.close()
